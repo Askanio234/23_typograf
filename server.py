f0b0e48dda489e76d8af94502c8d2c0e233cb8d3
Artem Chernyaev <askanio234@gmail.com>
2017-05-02 23:34:15 +0300

    Add basic integration of flask and webform via ajax

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def form():
-    return render_template('form.html')
 
-if __name__ == "__main__":
+@app.route('/', methods=['GET', 'POST'])
+def process_form():
+    if request.method == 'GET':
+        return render_template('form.html')
+    if request.method == 'POST':
+        users_text = request.form['text']
+        processed_text = users_text.upper()        
+        return processed_text
+
+
+if __name__ == '__main__':
     app.run()
